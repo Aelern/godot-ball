bf36f52c124f86d2a1edcc54d627c3b7ce657a04
Andrew Boyce <akboyce72@gmail.com>
2024-01-10 14:11:13 -0500

    Played with player movement statistics for more speedy and slippery. Decoupled acceleration and deceleration

--- player/player.gd
+++ player/player.gd
@@ -1,8 +1,9 @@
 class_name Player
 extends CharacterBody2D
 
-@export var acceleration: float = 50.0	#Rate speed increases
-@export var speed: int = 300	#Max speed in any direction
+@export var acceleration: float = 25.0	#Rate speed increases
+@export var deceleration: float = 10.0	#Rate speed increases
+@export var speed: int = 350	#Max speed in any direction
 
 @onready var hurtbox_component = $HurtboxComponent		#Used to kill the player
 
@@ -11,10 +12,16 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	var direction = Input.get_vector("left", "right", "up", "down")
 	if direction:
-		velocity.x = move_toward(velocity.x, speed * direction.x, acceleration)
-		velocity.y = move_toward(velocity.y, speed * direction.y, acceleration)
-	else:	#For now these two blocks of code do the same thing. Kept seperate in case I want acceleration and deceleration to be seperate values
-		velocity.x = move_toward(velocity.x, 0, acceleration)
-		velocity.y = move_toward(velocity.y, 0, acceleration)
+		if direction.x:
+			velocity.x = move_toward(velocity.x, speed * direction.x, acceleration)
+		else:
+			velocity.x = move_toward(velocity.x, 0, deceleration)		
+		if direction.y:
+			velocity.y = move_toward(velocity.y, speed * direction.y, acceleration)
+		else:
+			velocity.y = move_toward(velocity.y, 0, deceleration)
+	else:	#Technically same function, but with deceleration value
+		velocity.x = move_toward(velocity.x, 0, deceleration)
+		velocity.y = move_toward(velocity.y, 0, deceleration)
 
 	move_and_slide()
