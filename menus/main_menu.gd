a460283efca475081c75c5d19de8ae40b4829053
Andrew Boyce <akboyce72@gmail.com>
2024-01-18 15:34:08 -0500

    Manual merge attempted

--- menus/main_menu.gd
+++ menus/main_menu.gd
@@ -3,4 +3,5 @@ extends Node2D
 
 func _input(event):
 	if event.is_action_released("ui_accept"):
+		LevelMap.current_level = 1
 		get_tree().change_scene_to_file("res://stages/level_1.tscn")
--- objects/portal.gd
+++ objects/portal.gd
@@ -1,17 +1,24 @@
 extends Node2D
 
-@export var next_level: PackedScene		#The level the portal sends you to
-
 @onready var hitbox_component = $HitboxComponent	#The hitbox that is looking for the player's collision
+@onready var level_time = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	hitbox_component.hit_hurtbox.connect(change_level)
 
+func _process(delta):
+	level_time += delta
+
 #Run level completion effects then advance to the next level
 func change_level(hurtbox: HurtboxComponent):
-	if(next_level):
-		hurtbox.get_parent().tree_exiting.disconnect(hurtbox.get_parent().tree_exiting.get_connections()[0]["callable"])	#Stops an error being thrown when the game thinks the player is dying because the scene is unloading
-		get_tree().call_deferred("change_scene_to_packed", next_level)
+	PlayerStats.level_times[LevelMap.current_level-1][0] = snappedf(level_time, 0.01)
+	if level_time < PlayerStats.level_times[LevelMap.current_level-1][1]:
+		PlayerStats.level_times[LevelMap.current_level-1][1] = snappedf(level_time, 0.01)
+	if(LevelMap.current_level < len(LevelMap.levels)):
+		get_tree().call_deferred("change_scene_to_file", LevelMap.levels[LevelMap.current_level + 1])
+		LevelMap.current_level += 1
 	else:
 		print("You beat the game I guess")
+		PlayerStats.save_stats()
+		call_deferred("queue_free")
