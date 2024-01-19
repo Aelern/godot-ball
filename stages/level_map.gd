ca822ad33ae3858e3a75354f17ac19f30d648bdc
Andrew Boyce <akboyce72@gmail.com>
2024-01-19 15:40:19 -0500

    added level 5, need to check into array index error in change_level() btw

--- stages/level_map.gd
+++ stages/level_map.gd
@@ -4,7 +4,8 @@ var levels = {
 	1: "res://stages/level_1.tscn",
 	2: "res://stages/level_2.tscn",
 	3: "res://stages/level_3.tscn",
-	4: "res://stages/level_4.tscn"
+	4: "res://stages/level_4.tscn",
+	5: "res://stages/level_5.tscn"
 }
 
 var current_level = 0
