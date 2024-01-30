ede4dab792b1ad28d7b3dd0d161bb727c07faec4
Andrew Boyce <akboyce72@gmail.com>
2024-01-30 16:28:12 -0500

    Level Select works but style incomplete

---
+++ LevelSelectButton.gd
@@ -0,0 +1,20 @@
+class_name LevelSelectButton
+extends Button
+
+@export var level_number: int
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#When clicked send you to the correct level
+	button_up.connect(func():
+		get_tree().call_deferred("change_scene_to_file", LevelMap.levels[level_number])
+		LevelMap.current_level = level_number
+		)
+	#Display best time for the level (Probably needs to be it's own child label)
+	if len(PlayerStats.level_times) >= level_number:
+		text = " " + str(level_number) + " \n(" + str(PlayerStats.level_times[level_number-1][1]) + ")"
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
