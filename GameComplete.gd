5e8743132c2aac95757d9d728e920cf3ae1825d1
Andrew Boyce <akboyce72@gmail.com>
2024-02-01 16:13:22 -0500

    Added game complete screen

---
+++ GameComplete.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@onready var main_menu_button = $VBoxContainer/MainMenuButton
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	AudioStreamManager.bgmPlayer.stop()
+	main_menu_button.button_up.connect(func():
+		get_tree().call_deferred("change_scene_to_file", LevelMap.main_menu)
+		)
